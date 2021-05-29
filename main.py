d9731c56ea7148e6c94aa6941a7e2785ff5830b4
Faunt0 <kllex028@gmail.com>
2021-05-29 17:25:57 +0200

    Changed the trails to a more oop friendly way

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from decimal import *
 #constants
 G = 6.67 * 10**-11
 # defining a certain radius of where to look for different particles to interact with
-r = 2
+# to be implemented
+#r = 2
 
 
 
@@ -17,9 +18,6 @@ def vectorsAdd(*vectors):
             newVector[j] = newVector[j] + list(vectors)[i][j]
    
     return newVector
-
-
-
 
 def vectorMultiply(vector, l):
     newVector = []
@@ -41,7 +39,6 @@ def vectorsCorner(v1, v2, t):
     elif dx == 0 and dy < 0:
         #when its directly above the mass particle
         corner = 90
-#        corner = 0
     elif dx == 0 and dy > 0:
         #when its directly below the mass particle
         corner = 90
@@ -73,6 +70,8 @@ class Particle:
         self.sv = sv
         self.mv = mv
         self.mass = mass
+        self.xcoords = [self.sv[0]]
+        self.ycoords = [self.sv[1]]
     def position(self, t):
         mvA = vectorMultiply(self.mv, t)
 
@@ -88,10 +87,7 @@ class Particle:
 
 p1 = Particle([-1,0], [0.80,-2.0], 80000)
 p2 = Particle([10,10], [0,0], 9000000)
-p3 = Particle([-2,5], [0,0], 10000000)
-#print(p1.position(2))
-#print(vectorMultiply(p1.mv, 2))
-print(vectorsCorner(p1, p2, 0))
+p3 = Particle([20,15], [0,0], 10000000)
 
 # The Inter Particle Force Calculator, at your service!
 def IPFC (p1, p2, t):
@@ -100,7 +96,6 @@ def IPFC (p1, p2, t):
         r += (p1.position(t)[i] - p2.position(t)[i])**2
    
 
-    #f = G * (p1.mass * p2.mass) / r
     f = G * (p1.mass * p2.mass) / r
     fx = math.cos(math.radians(vectorsCorner(p1, p2, t))) * f
     fy = math.sin(math.radians(vectorsCorner(p1, p2, t))) * f
@@ -108,48 +103,36 @@ def IPFC (p1, p2, t):
 
 
 # The rendering of the points and trails. before this all the forces being applied should be calculated
-xcoords = []
-ycoords = []
 
 plt.plot([p1.sv[0]], [p1.sv[1]], 'bo' )
-print(f'p1: {p1.sv}\tp2: {p2.sv}')
-pitch = 0.001
+
+pitch = 0.0001
 nstep = round(1/pitch)
 for t in range(0, 1):
-    #newPos = p1.position(t)
     stepPitch = t
     for step in range(0, nstep):
                 
         force = IPFC(p1, p2, stepPitch)
-        #newPos = vectorsAdd(p1.position(stepPitch), [force[1], force[2]])
         newPos = vectorsAdd(p1.sv, p1.mv, [force[1], force[2]])
         p1.sv = newPos
         p1.mv = vectorsAdd(p1.mv, [force[1], force[2]])
         
-        xcoords.append(float(newPos[0]))
-        ycoords.append(float(newPos[1]))
+        p1.xcoords.append(float(newPos[0]))
+        p1.ycoords.append(float(newPos[1]))
 
-        print(f'step = {step}\tnewPos1: {newPos}\tforce = {force[0]}\tfx = {force[1]}\tfy = {force[2]}')
+#        print(f'step = {step}\tnewPos1: {newPos}\tforce = {force[0]}\tfx = {force[1]}\tfy = {force[2]}')
         
 #        if pitch/force[0] < 1:
 #            forcedpitch = pitch/force[0]
 #            stepPitch += forcedpitch
 #        else:
 #            stepPitch += pitch
+        
         stepPitch += pitch
 
 
-#print(vectorsCorner(p1, p2, 0))
-#fig, ax = plt.subplots()
-#ax.plot(xcoords, ycoords, label="particle movement")
-#ax.plot([p2.sv[0]], [p2.sv[1]], label="Big mass")
-#ax.legend()
-#ax.title("particle movements")
-# just for the ease of mind, I remove the last value since it seems to fuck up often around there.
-#xcoords[len(xcoords)-1] = 0
-#ycoords[len(ycoords)-1] = 0
 
-plt.plot(xcoords, ycoords, label="particle movement")
+plt.plot(p1.xcoords, p1.ycoords, label="particle movement")
 plt.plot([p2.sv[0]], [p2.sv[1]], 'ro' )
 plt.legend()
 plt.title("particle movements")
