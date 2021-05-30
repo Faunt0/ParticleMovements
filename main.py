730950e27bb5ef9b5f8dc63e68c3400c423850ed
Faunt0 <kllex028@gmail.com>
2021-05-31 00:53:04 +0200

    more gifs, better file.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import math
+import random
 import matplotlib.pyplot as plt
 #from matplotlib.animation import FuncAnimation, PillowWriter
 import numpy as np
@@ -7,10 +8,10 @@ from decimal import *
 
 #constants
 G = 6.67 * 10**-11
+c = 299792458
 # defining a certain radius of where to look for different particles to interact with
 # to be implemented
 #r = 2
-
 
 
 def vectorsAdd(*vectors):
@@ -67,12 +68,15 @@ def vectorsCorner(v1, v2, t):
     return corner
 
 class Particle:
-    def __init__(self, sv, mv, mass):
+    def __init__(self, sv, mv, mass, ismp):
         self.sv = sv
         self.mv = mv
         self.mass = mass
         self.xcoords = [self.sv[0]]
         self.ycoords = [self.sv[1]]
+        self.rs = (2 * G * self.mass) / (c**2)
+        self.rs = 30
+        self.ismp = False
     def position(self, t):
         mvA = vectorMultiply(self.mv, t)
 
@@ -89,79 +93,129 @@ def IPFC (p1, mp2, t):
     r = 0
     for i in range(0, len(p1.position(t))):
         r += (p1.position(t)[i] - mp2.position(t)[i])**2
-   
+  
+    #doesn't work properly
+    if math.sqrt(r) < mp2.rs:
+#       print('the particle will be sucked in ;')
+        #p1.sv = mp2.sv
+        #p1.mv = [0, 0]
+        del p1
+        return [0,0,0]
+    else:
+        f = G * (p1.mass * mp2.mass) / r
+        fx = math.cos(math.radians(vectorsCorner(p1, mp2, t))) * f
+        fy = math.sin(math.radians(vectorsCorner(p1, mp2, t))) * f
+#    else:
+#        f = G * (p1.mass * mp2.mass) / r
+#        fx = math.cos(math.radians(vectorsCorner(p1, mp2, t))) * f
+#        fy = math.sin(math.radians(vectorsCorner(p1, mp2, t))) * f
+        return [f, fx, fy]
 
-    f = G * (p1.mass * mp2.mass) / r
-    fx = math.cos(math.radians(vectorsCorner(p1, mp2, t))) * f
-    fy = math.sin(math.radians(vectorsCorner(p1, mp2, t))) * f
-    return [f, fx, fy]
 
 
 # Moving Particles
-p1 = Particle([-5,-9], [1.70,-0.7], 80000)
+#p1 = Particle([-5,-9], [1.70,-0.7], 80000)
 #p1 = Particle([-1,0], [-1.15,2.0], 80000)
-orbitalParticles = [p1]
-
-# Mass particles
-p2 = Particle([10,10], [0,0], 10050000)
-p3 = Particle([100,100], [0,0], 1000000)
-#p4 = Particle([50,-90], [0,0], 20000000)
+#orbitalParticles = [
+#    #Particle([10,90], [3.0,1.3], 800000),
+#    #Particle([-20,-13], [1.00,-3.7], 80000),
+#    Particle([150,-50], [-0.18,-0.1], 80000),
+#    #Particle([-50,-100], [-2.1,-0.5], 840000),
+#    Particle([-100,-1], [-0.1,-0.1], 840000),
+#    Particle([10,90], [-2.5,-0.1], 850000),
+#]
+#orbitalParticles = []
+orbitalParticles = [Particle([random.randrange(-150, 150),random.randrange(-150, 150)], [random.randrange(-10, 10), random.randrange(-10, 10)], random.randint(800000, 1500000), False) for i in range(0, 50)]
+#orbitalParticles.append(Particle([150,150], [-1,-1], 166616))#between 80000-1000000))
+#orbitalParticles = [Particle([-70,-100-i*10], [1.5, -0.5], 800000) for i in range(0, 20)]
+#orbitalParticles = [Particle([70*math.cos(i),-600-80*math.sin(i)], [1.5, -0.5], 800000) for i in range(0, 20)]
 
 massParticles = [
-        Particle([10,10], [0,0], 10050000), 
-        Particle([100,100], [0,0], 900000),
-        #p4,
-    ]
-#massParticles = [p2]
-
+    Particle([10,10], [0,0], 4050000, True), 
+    Particle([150,100], [0,0], 10050000, True),
+#    Particle([-100,120], [0,0], 3600000),
+]
+massParticles = [
+#    Particle([150,100], [0,0], 10050000, True),
+#    Particle([300,-400], [0,0], 10500000, True),
+#    Particle([-100,120], [0,0], 3600000, True),
+#    Particle([400,400], [0,0], 100500000, True),
+    Particle([-400,400], [0,0], 104500000, True),
+    Particle([200,200], [0,0], 104500000, True),
+    Particle([200,-200], [0,0], 104500000, True),
+]
 
 
 # The rendering of the points and trails. before this all the forces being applied should be calculated
 
+grandPitch = 100
+pitch = 0.05
+def particleMoves(p):
+    nstep = round(1/pitch)
+    for t in range(0, grandPitch):
+        stepPitch = t
+        for step in range(0, nstep):
+            force = [0,0]
+            for mp in massParticles:
+                force = vectorsAdd(force, [IPFC(p, mp, stepPitch)[1], IPFC(p, mp, stepPitch)[2]])
+            #newPos = vectorsAdd(p1.sv, p1.mv, [force[1], force[2]])
+            newPos = vectorsAdd(p.sv, p.mv, [force[0], force[1]])
+            p.sv = newPos
+            #p1.mv = vectorsAdd(p1.mv, [force[1], force[2]])
+            p.mv = vectorsAdd(p.mv, [force[0], force[1]])
+            
+            p.xcoords.append(float(newPos[0]))
+            p.ycoords.append(float(newPos[1]))
+    
+            stepPitch += pitch
+    #        print(f'step = {step}\tnewPos1: {newPos}\tforce = {force[0]}\tfx = {force[1]}\tfy = {force[2]}')
 
-pitch = 0.0001
-nstep = round(1/pitch)
-for t in range(0, 1):
-    stepPitch = t
-    for step in range(0, nstep):
-        force = [0,0]
-        for mp in massParticles:
-            force = vectorsAdd(force, [IPFC(p1, mp, stepPitch)[1], IPFC(p1, mp, stepPitch)[2]])
-        #newPos = vectorsAdd(p1.sv, p1.mv, [force[1], force[2]])
-        newPos = vectorsAdd(p1.sv, p1.mv, [force[0], force[1]])
-        p1.sv = newPos
-        #p1.mv = vectorsAdd(p1.mv, [force[1], force[2]])
-        p1.mv = vectorsAdd(p1.mv, [force[0], force[1]])
-        
-        p1.xcoords.append(float(newPos[0]))
-        p1.ycoords.append(float(newPos[1]))
+for p in orbitalParticles:
+    print(f'p: {orbitalParticles.index(p)}\tsv = {p.sv}\tmv = {p.mv}\tmass = {p.mass}')
+    particleMoves(p)
 
-        stepPitch += pitch
-#        print(f'step = {step}\tnewPos1: {newPos}\tforce = {force[0]}\tfx = {force[1]}\tfy = {force[2]}')
-
+colors = 'bgcmyk' * 10
+colors = 'wm' * 140
 fig, ax = plt.subplots()
-x, y = [], []
-line, = ax.plot(p1.xcoords, p1.ycoords, 'm.')
+ax.set_facecolor('k')
+#line, = ax.plot(p1.xcoords, p1.ycoords, 'm.')
+lines = [ax.plot(p.xcoords, p.ycoords, f'{colors[orbitalParticles.index(p)]}.')[0] for p in orbitalParticles]
 massPoints = [ax.plot(mp.sv[0], mp.sv[1], 'rx') for mp in massParticles]
 
 #animation proces
 def init():
-    ax.set_xlim(-100, 110)
-    ax.set_ylim(-70, 125)
-    return line, massPoints,
+   #ax.set_xlim(-220, 200)
+   #ax.set_ylim(-190, 250)
+   #ax.set_xlim(-150, 250)
+   #ax.set_ylim(-100, 200)
 
-def update(frame):
-    x.append(p1.xcoords[frame])
-    y.append(p1.ycoords[frame])
-    line.set_data(x, y)
-    return line,
+#   ax.set_xlim(-250, 375)
+#   ax.set_ylim(-125, 160)
 
-def animate(save=False):
-    ani = animation.FuncAnimation(fig, update, len(p1.xcoords), init_func=init, interval=20, blit=True, save_count=50)
-    if save == True:
-        ani.save('animgif5.gif')
+    ax.set_xlim(-320, 780)
+    ax.set_ylim(-360, 775)
+    
+    ax.set_xlim(-1000, 780)
+    ax.set_ylim(-500, 775)
 
-#animate()
+
+
+#    ax.set_xlim(-50, 300)
+#    ax.set_ylim(-100, 250)
+
+
+
+
+    return lines, massPoints,
+
+def update(frame, particles, lines):
+    for line, p in zip(lines, particles):
+        line.set_data(p.xcoords[0:frame], p.ycoords[0:frame])
+    return lines
+
+ani = animation.FuncAnimation(fig, update, round(grandPitch/pitch), fargs=(orbitalParticles, lines), init_func=init, interval=20, save_count=50)
+#ani.save('animgif8.gif')
+
 
 #plt.plot(p1.xcoords, p1.ycoords, label="particle movement")
 #plt.plot([p2.sv[0]], [p2.sv[1]], 'ro' )
