a31b4f742801440118b50f23f1aad080fafdd4dc
Faunt0 <kllex028@gmail.com>
2021-05-31 13:02:05 +0200

    more pictures and cleanup

--- main.py
+++ main.py
@@ -111,45 +111,6 @@ def IPFC (p1, mp2, t):
 #        fy = math.sin(math.radians(vectorsCorner(p1, mp2, t))) * f
         return [f, fx, fy]
 
-
-
-# Moving Particles
-#p1 = Particle([-5,-9], [1.70,-0.7], 80000)
-#p1 = Particle([-1,0], [-1.15,2.0], 80000)
-#orbitalParticles = [
-#    #Particle([10,90], [3.0,1.3], 800000),
-#    #Particle([-20,-13], [1.00,-3.7], 80000),
-#    Particle([150,-50], [-0.18,-0.1], 80000),
-#    #Particle([-50,-100], [-2.1,-0.5], 840000),
-#    Particle([-100,-1], [-0.1,-0.1], 840000),
-#    Particle([10,90], [-2.5,-0.1], 850000),
-#]
-#orbitalParticles = []
-orbitalParticles = [Particle([random.randrange(-150, 150),random.randrange(-150, 150)], [random.randrange(-10, 10), random.randrange(-10, 10)], random.randint(800000, 1500000), False) for i in range(0, 50)]
-#orbitalParticles.append(Particle([150,150], [-1,-1], 166616))#between 80000-1000000))
-#orbitalParticles = [Particle([-70,-100-i*10], [1.5, -0.5], 800000) for i in range(0, 20)]
-#orbitalParticles = [Particle([70*math.cos(i),-600-80*math.sin(i)], [1.5, -0.5], 800000) for i in range(0, 20)]
-
-massParticles = [
-    Particle([10,10], [0,0], 4050000, True), 
-    Particle([150,100], [0,0], 10050000, True),
-#    Particle([-100,120], [0,0], 3600000),
-]
-massParticles = [
-#    Particle([150,100], [0,0], 10050000, True),
-#    Particle([300,-400], [0,0], 10500000, True),
-#    Particle([-100,120], [0,0], 3600000, True),
-#    Particle([400,400], [0,0], 100500000, True),
-    Particle([-400,400], [0,0], 104500000, True),
-    Particle([200,200], [0,0], 104500000, True),
-    Particle([200,-200], [0,0], 104500000, True),
-]
-
-
-# The rendering of the points and trails. before this all the forces being applied should be calculated
-
-grandPitch = 100
-pitch = 0.05
 def particleMoves(p):
     nstep = round(1/pitch)
     for t in range(0, grandPitch):
@@ -170,19 +131,6 @@ def particleMoves(p):
             stepPitch += pitch
     #        print(f'step = {step}\tnewPos1: {newPos}\tforce = {force[0]}\tfx = {force[1]}\tfy = {force[2]}')
 
-for p in orbitalParticles:
-    print(f'p: {orbitalParticles.index(p)}\tsv = {p.sv}\tmv = {p.mv}\tmass = {p.mass}')
-    particleMoves(p)
-
-colors = 'bgcmyk' * 10
-colors = 'wm' * 140
-fig, ax = plt.subplots()
-ax.set_facecolor('k')
-#line, = ax.plot(p1.xcoords, p1.ycoords, 'm.')
-lines = [ax.plot(p.xcoords, p.ycoords, f'{colors[orbitalParticles.index(p)]}.')[0] for p in orbitalParticles]
-massPoints = [ax.plot(mp.sv[0], mp.sv[1], 'rx') for mp in massParticles]
-
-#animation proces
 def init():
    #ax.set_xlim(-220, 200)
    #ax.set_ylim(-190, 250)
@@ -213,11 +161,62 @@ def update(frame, particles, lines):
         line.set_data(p.xcoords[0:frame], p.ycoords[0:frame])
     return lines
 
-#ani = animation.FuncAnimation(fig, update, round(grandPitch/pitch), fargs=(orbitalParticles, lines), init_func=init, interval=20, save_count=50)
-#ani.save('animgif8.gif')
-
-
-#plt.plot(p1.xcoords, p1.ycoords, label="particle movement")
-#plt.plot([p2.sv[0]], [p2.sv[1]], 'ro' )
-#plt.legend()
-plt.show()
+if __name__ == "__main__":
+    print('calling main')
+    
+    # Moving Particles
+    #p1 = Particle([-5,-9], [1.70,-0.7], 80000)
+    #p1 = Particle([-1,0], [-1.15,2.0], 80000)
+    orbitalParticles = [
+        #Particle([10,90], [3.0,1.3], 800000, False),
+        #Particle([-20,-13], [1.00,-3.7], 80000, False),
+        Particle([150,-50], [-3.18,5.1], 80000, False),
+        #Particle([-50,-100], [-2.1,-0.5], 840000, False),
+        Particle([-100,-1], [-0.1,-0.1], 840000, False),
+        Particle([-100,-1], [-2.1,-7.1], 840000, False),
+        Particle([10,90], [-2.5,-0.1], 850000, False),
+    ]
+    #orbitalParticles = []
+#    orbitalParticles = [Particle([random.randrange(-150, 150),random.randrange(-150, 150)], [random.randrange(-10, 10), random.randrange(-10, 10)], random.randint(800000, 1500000), False) for i in range(0, 50)]
+    #orbitalParticles.append(Particle([150,150], [-1,-1], 166616))#between 80000-1000000))
+    #orbitalParticles = [Particle([-70,-100-i*10], [1.5, -0.5], 800000) for i in range(0, 20)]
+    #orbitalParticles = [Particle([70*math.cos(i),-600-80*math.sin(i)], [1.5, -0.5], 800000) for i in range(0, 20)]
+    
+    massParticles = [
+        Particle([10,10], [0,0], 4050000, True), 
+        Particle([150,100], [0,0], 10050000, True),
+    #    Particle([-100,120], [0,0], 3600000),
+    ]
+    massParticles = [
+    #    Particle([150,100], [0,0], 10050000, True),
+    #    Particle([300,-400], [0,0], 10500000, True),
+    #    Particle([-100,120], [0,0], 3600000, True),
+    #    Particle([400,400], [0,0], 100500000, True),
+#        Particle([-400,400], [0,0], 104500000, True),
+        Particle([200,200], [0,0], 104500000, True),
+        Particle([200,-200], [0,0], 104500000, True),
+    ]
+    
+    
+    grandPitch = 10
+    pitch = 0.001
+    for p in orbitalParticles:
+        print(f'p: {orbitalParticles.index(p)}\tsv = {p.sv}\tmv = {p.mv}\tmass = {p.mass}')
+        particleMoves(p)
+    
+    colors = 'bgcmyk' * 10
+    colors = 'wgm' * 140
+    fig, ax = plt.subplots()
+    ax.set_facecolor('k')
+    lines = [ax.plot(p.xcoords, p.ycoords, f'{colors[orbitalParticles.index(p)]}.')[0] for p in orbitalParticles]
+    massPoints = [ax.plot(mp.sv[0], mp.sv[1], 'rx') for mp in massParticles]
+    
+    #animation proces
+    #ani = animation.FuncAnimation(fig, update, round(grandPitch/pitch), fargs=(orbitalParticles, lines), init_func=init, interval=20, save_count=50)
+    #ani.save('animgif9.gif')
+    
+    
+    #plt.legend()
+    plt.show()
+    
+    
